68b4cd174f05a8158f040540f3384a8dc312b18c
swt <sharda.adake@standardwings.com>
2024-06-14 18:01:19 +0530

    sms

--- app/Libraries/SmsApis.php
+++ app/Libraries/SmsApis.php
@@ -8,7 +8,9 @@ class SmsApis{
 	public function post_sms($mobile,$otp)
 	{
 		if(!empty($otp)&& !empty($mobile)){
-			$url ="http://site.ping4sms.com/api/smsapi?key=aebaa88ce79128bcb31282dcdb6507d5&route=2&sender=YTCRUN&number=$mobile&sms=Dear%20Customer,%20Your%20OTP%20is%20$otp.%20Thank%20You%C2%A0-%C2%A0YTCRUN&templateid=1707165967704553648";
+			// $url ="http://site.ping4sms.com/api/smsapi?key=aebaa88ce79128bcb31282dcdb6507d5&route=2&sender=YTCRUN&number=$mobile&sms=Dear%20Customer,%20Your%20OTP%20is%20$otp.%20Thank%20You%C2%A0-%C2%A0YTCRUN&templateid=1707165967704553648";
+
+			$url = "http://site.ping4sms.com/api/smsapi?key=aebaa88ce79128bcb31282dcdb6507d5&route=2&sender=YTCRUN&number=$mobile&sms=Dear%20Customer,%20Your%20Login%20OTP%20for%20the%20$otp%20Thank%20you%20-%20YOUTOOCANRUN&templateid=1707170556227384012";
 		if( function_exists("curl_init")){
 		$ch = curl_init();
 		curl_setopt ( $ch, CURLOPT_URL, $url );
