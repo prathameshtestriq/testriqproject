865bb9508f8223f7496789521979e71239c9feec
swtprathmesh <prathameshjagtap@standardwings.com>
2024-07-03 11:34:04 +0530

    create new cron

---
+++ app/Console/Commands/CommonFilesRemove.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use File;
+
+class CommonFilesRemove extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:common-files-remove';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Excell Files Remove';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \Log::info("is log working");
+
+        $current_day = date('Ymd');
+
+        $path = public_path('attendee_details_excell');
+        // dd($path);
+
+        $directories = File::directories($path);
+
+        foreach ($directories as $directory) { 
+
+            $file = pathinfo($directory);
+            
+            if($file['filename'] < $current_day){
+               
+                if (file_exists($directory)) {
+
+                    File::deleteDirectory($directory);
+                }
+             }
+
+        }
+
+    }
+
+  
+}
